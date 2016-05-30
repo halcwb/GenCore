bb511ec1661c0cb796ff7c149c138b9cfffaba78
Casper Bollen <halcwb@gmail.com>
2016-05-30 15:09:27 +0200

    Trying again without app.config

--- tests/Informedica.GenCore.Tests/BuildTest.fs
+++ tests/Informedica.GenCore.Tests/BuildTest.fs
@@ -1,17 +1,18 @@
-﻿namespace Informedica.GenCore.Tests
+﻿namespace Informedica.GenUtils.Tests
 
 open Swensen.Unquote
 open NUnit.Framework
 open FsCheck
 open FsCheck.NUnit
 
-open Informedica.GenCore.Lib
+open Informedica.GenUtils.Lib
 
 [<TestFixture>]
 type ``Test that build was succesfull`` () =
     
     [<Test>]
     member x.``Always pass test`` () = ()
+
 
     [<Test>]
     member x.``Unquote is working`` () =
@@ -20,3 +21,4 @@ type ``Test that build was succesfull`` () =
     [<Property>]
     member x.``FsCheck is working`` () =
         fun b -> if b then true else true
+
